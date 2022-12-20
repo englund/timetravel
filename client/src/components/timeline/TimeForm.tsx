a66ebf896740f8d5c9dffc149b57235067da7921
Victor Englund <victor.englund@gmail.com>
2022-12-20 13:07:01 +0100

    add datepicker to form and make form pretty

--- client/src/components/timeline/TimeForm.tsx
+++ client/src/components/timeline/TimeForm.tsx
@@ -1,7 +1,9 @@
 import { FC, useState } from "react";
 
+import { Stack } from "@mui/material";
 import Button from "@mui/material/Button/Button";
 import TextField from "@mui/material/TextField/TextField";
+import { DatePicker } from "@mui/x-date-pickers";
 
 interface Props {
   addTime: (hours: number, date: Date) => void;
@@ -9,15 +11,33 @@ interface Props {
 
 const TimeForm: FC<Props> = ({ addTime }) => {
   const [hours, setHours] = useState(8);
+  const [date, setDate] = useState<Date>(new Date());
+
   return (
-    <>
+    <Stack direction="row" spacing={2}>
       <TextField
         type="number"
+        label="Antal timmar"
         value={hours}
         onChange={(e) => setHours(+e.target.value)}
       />
-      <Button onClick={() => addTime(hours, new Date())}>Lägg till</Button>
-    </>
+      <DatePicker
+        label="Datum"
+        value={date}
+        onChange={(newDate) => {
+          if (!newDate) return;
+          setDate(newDate);
+        }}
+        renderInput={(params) => <TextField {...params} />}
+      />
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => addTime(hours, date)}
+      >
+        Lägg till
+      </Button>
+    </Stack>
   );
 };
 
