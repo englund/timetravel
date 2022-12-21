059de95a6d698bde4a6e0b42199b491d696a6f93
Victor Englund <victor.englund@gmail.com>
2022-12-21 16:05:16 +0100

    add react query to app

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,3 +1,5 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import CssBaseline from "@mui/material/CssBaseline/CssBaseline";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
@@ -7,14 +9,21 @@ import svLocale from "date-fns/locale/sv";
 import Layout from "@/layout/Layout";
 import Timeline from "@/pages/Timeline";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={svLocale}>
-      <CssBaseline />
-      <Layout>
-        <Timeline />
-      </Layout>
-    </LocalizationProvider>
+    <QueryClientProvider client={queryClient}>
+      <LocalizationProvider
+        dateAdapter={AdapterDateFns}
+        adapterLocale={svLocale}
+      >
+        <CssBaseline />
+        <Layout>
+          <Timeline />
+        </Layout>
+      </LocalizationProvider>
+    </QueryClientProvider>
   );
 }
 
