cc160827b936f72292f604aa34a19aedfcbb03bc
Victor Englund <victor.englund@gmail.com>
2022-12-21 16:57:04 +0100

    skip useMemo for group by week

--- packages/app/src/hooks/useGroupByWeek.tsx
+++ packages/app/src/hooks/useGroupByWeek.tsx
@@ -1,22 +1,16 @@
-import { useMemo } from "react";
-
 import { compareAsc } from "date-fns";
 
 import { Time } from "@/models/time";
 import { formatDate } from "@/utils/date";
 
 export const useGroupByWeek = (times: Time[]) =>
-  useMemo(
-    () =>
-      times
-        .sort((a, b) => compareAsc(a.date, b.date))
-        .reduce<Record<string, Time[]>>((grouped, time) => {
-          const week = formatDate(time.date, "I");
-          if (!grouped[week]) {
-            grouped[week] = [];
-          }
-          grouped[week] = [...grouped[week], time];
-          return grouped;
-        }, {}),
-    [times]
-  );
+  times
+    .sort((a, b) => compareAsc(a.date, b.date))
+    .reduce<Record<string, Time[]>>((grouped, time) => {
+      const week = formatDate(time.date, "I");
+      if (!grouped[week]) {
+        grouped[week] = [];
+      }
+      grouped[week] = [...grouped[week], time];
+      return grouped;
+    }, {});
